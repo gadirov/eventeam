2732cc64ecee12fdb69e5aa6a6bdb4032e4231c8
Sabir Huseynov <005539@ibar.int>
2024-01-04 14:21:54 +0400

    add component folder

---
+++ src/components/layout/about.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+export const About = () => {
+  return <Box>this is view page</Box>;
+};
---
+++ src/components/login/signIn/index.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+export const SignIn = () => {
+  return <Box>this is view page</Box>;
+};
