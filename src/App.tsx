b8141bd83ee43423185c887fad90d42af6b54bb2
Rufat Gadirov <gadirov.567@gmail.com>
2024-01-04 12:14:50 +0400

    changed js file to ts file, add Header section

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Box, HStack, Heading, ListItem, UnorderedList } from "@chakra-ui/react";
+
+
+function App() {
+  return (
+    <Box  w="100vw" minH="100vh">
+      <HStack  display="flex" justifyContent="space-between" p="10px 64px" bgColor="lightgray">
+          <Heading>Eventeam</Heading>
+          <UnorderedList listStyleType="none" display="flex" gap="20px" fontWeight="bold">
+            <ListItem>AZ</ListItem>
+            <ListItem>EN</ListItem>
+            <ListItem>RU</ListItem>
+          </UnorderedList>
+      </HStack>
+      <Heading display="flex" justifyContent="center" alignItems="center" height="90vh">Eventeam</Heading>
+    </Box>
+  );
+}
+
+export default App;
