743c833b4416328abe11ed5078b6207d812a140d
Rufat Gadirov <gadirov.567@gmail.com>
2024-01-05 22:45:24 +0400

    make sign in page

--- src/components/layout/home/FeatureEvents.tsx
+++ src/components/layout/home/FeatureEvents.tsx
@@ -1,9 +1,10 @@
-import { Box, Image, Text, Heading, Button , Link } from "@chakra-ui/react";
+import { Box, Image, Text, Heading, Button , Link, Divider } from "@chakra-ui/react";
 import React from "react";
 
 export default function FeaturedEvents() {
     return (
-        <Box m="60px 0px"  >
+        <Box m="10px 0px 120px 0"  >
+            <Divider />
             <Box w="85%" margin="auto" padding="120px 0px" >
                 <Box display="flex" justifyContent="center" alignItems="center" gap="160px" >
                     <Box display="flex" flexDirection="column" alignItems="flex-start" gap="24x"  >
@@ -29,19 +30,20 @@ export default function FeaturedEvents() {
                                 alignItems="center"
                                 gap="10px"
                                 borderRadius="5px"
-                                background="#2A1955"
+                                background="#8f64ff"
                                 color="#FEFEFF"
                             >Learn More</Button>
                         </Box>
                     </Box>
                     <Box >
                         <Link>
-                            <Image src="./assests/FeaturedEvents-Images/EventImg.png" alt='Dan Abramov' />
+                            <Image src="./assests/FeaturedEvents-Images/EventImg.png" />
                         </Link>
 
                     </Box>
                 </Box>
             </Box>
+            <Divider/>
         </Box>
     )
 }
