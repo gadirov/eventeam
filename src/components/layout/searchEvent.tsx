823e6265ab492a9506d96ebf0ca5f34f3150bd29
ismayilhasan <ismayilrha@code.edu.az>
2024-01-07 00:22:30 +0400

    dvide card component and added search and filter area

--- src/components/layout/searchEvent.tsx
+++ src/components/layout/searchEvent.tsx
@@ -1,8 +1,34 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Container, Flex, Input, Select } from "@chakra-ui/react";
+import React from "react";
 
 export default function SearchEvent() {
   return (
-    <Box pt="200px">searchEvent</Box>
-  )
+    <Box pt="200px">
+      <Container maxW="1170px">
+        <Input
+          py={"20px"}
+          size={"lg"}
+          placeholder="Search Here..."
+          fontWeight={"bold"}
+        />
+       <Flex justify={"flex-start"} gap={"20px"} my={"40px"}>
+       <Select placeholder="Month" fontWeight={"bold"} w={"141px"} bg={"#F2F4F7"}>
+          <option value="option1">Option </option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Category" fontWeight={"bold"} w={"141px"} bg={"#F2F4F7"}>
+          <option value="option1">Option </option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+        <Select placeholder="Free" fontWeight={"bold"} w={"141px"} bg={"#F2F4F7"}>
+          <option value="option1">Option </option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </Select>
+       </Flex>
+      </Container>
+    </Box>
+  );
 }
