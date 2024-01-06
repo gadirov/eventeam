d7c71dc5cc498a7b1d9f2192b5e2d691c265214e
Rufat Gadirov <gadirov.567@gmail.com>
2024-01-06 12:53:37 +0400

    make event.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import About from "./components/layout/about.tsx";
 import Contact from "./components/layout/contact.tsx";
 import LayoutLogin from "./components/login/index.tsx";
 import SignIn from "./components/login/signIn/index.tsx";
+import Event from "./components/layout/event.tsx";
 function App() {
   const { data } = useDetails();
   console.log("-----", data);
@@ -19,7 +20,7 @@ function App() {
             <Route index element={<Home/>}/>
             <Route path="about" element={<About/>}/>
             <Route path="contact" element={<Contact/>}/>
-            {/* <Route path="events" element={</>}/> */}
+            <Route path="events" element={<Event />}/>
         </Route>
         <Route path="/sign-in" element={<LayoutLogin />}>
           <Route index  element={<SignIn />}/> 
---
+++ src/components/layout/event.tsx
@@ -0,0 +1,12 @@
+import { Box, Heading } from '@chakra-ui/react';
+import React from 'react'
+
+function Event() {
+  return (
+    <Box pt="100px">
+        <Heading>Create an Event</Heading>
+    </Box>
+  )
+}
+
+export default Event;
