7cb7b67353a483410633342daaa038d81f52cbd0
ismayilhasan <ismayilrha@code.edu.az>
2024-01-07 11:07:27 +0400

    added filter search ui and functionality,enums for mohths and fixing type issues

--- src/components/layout/searchEvent.tsx
+++ src/components/layout/searchEvent.tsx
@@ -1,7 +1,35 @@
-import { Box, Container, Flex, Input, Select } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Container,
+  Flex,
+  Input,
+  Select,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import React from "react";
-
+import CardItem from "../Card/CartdItem.tsx";
+import data from "./home/eventdata.json";
+import { Months } from "../../const.ts";
+import { IEventData } from "../../model.ts";
 export default function SearchEvent() {
+  const [searchField, setSearchField] = React.useState<string>("");
+  const [MonthList] = React.useState<string[]>(Object.keys(Months));
+  const [eventData,setEventData] = React.useState<IEventData[]>()
+  const handleChange = e => {
+    setSearchField(e.target.value);
+    
+
+  };
+
+  React.useEffect(() => {
+    setEventData(data)
+  },[])
+  const handleClearFilter = (event) => {
+    setEventData(data)
+    setSearchField("")
+    
+  }
   return (
     <Box pt="200px">
       <Container maxW="1170px">
@@ -10,24 +38,55 @@ export default function SearchEvent() {
           size={"lg"}
           placeholder="Search Here..."
           fontWeight={"bold"}
+          onChange={handleChange}
         />
-       <Flex justify={"flex-start"} gap={"20px"} my={"40px"}>
-       <Select placeholder="Month" fontWeight={"bold"} w={"141px"} bg={"#F2F4F7"}>
-          <option value="option1">Option </option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
-        <Select placeholder="Category" fontWeight={"bold"} w={"141px"} bg={"#F2F4F7"}>
-          <option value="option1">Option </option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
-        <Select placeholder="Free" fontWeight={"bold"} w={"141px"} bg={"#F2F4F7"}>
-          <option value="option1">Option </option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
-       </Flex>
+        <Flex justify={"flex-start"} gap={"20px"} my={"40px"}>
+          <Select
+            placeholder="Month"
+            fontWeight={"bold"}
+            w={"141px"}
+            bg={"#F2F4F7"}
+          >
+            {MonthList.map((value) => (
+              <option value={value}>{value}</option>
+            ))}
+           
+  
+          </Select>
+          <Select
+            placeholder="Category"
+            fontWeight={"bold"}
+            w={"141px"}
+            bg={"#F2F4F7"}
+          >
+            <option value="option1">Option </option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          <Select
+            placeholder="Free"
+            fontWeight={"bold"}
+            w={"141px"}
+            bg={"#F2F4F7"}
+          >
+            <option value="option1">Option </option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </Select>
+          <Button onClick={handleClearFilter} bg={"blue"} color={"#fff"}>Clear Filter</Button>
+        </Flex>
+
+        <SimpleGrid columns={[1, 2, 3]} spacing="60px">
+          {eventData?eventData
+            .filter((event) => {
+              return event.title
+                .toLowerCase()
+                .includes(searchField.toLowerCase());
+            })
+            .map((event) => (
+              <CardItem {...event} />
+            )) : "loading..."}
+        </SimpleGrid>
       </Container>
     </Box>
   );
