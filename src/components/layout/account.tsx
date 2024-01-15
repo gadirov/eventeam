f57330cc488437fd419b5b7947824048b30c41dd
Rufat Gadirov <gadirov.567@gmail.com>
2024-01-15 20:51:05 +0400

    change account

--- src/components/layout/account.tsx
+++ src/components/layout/account.tsx
@@ -95,82 +95,78 @@ const Account = () => {
           borderRadius={"10px"}
           p={"60px"}
         >
-      
-
           <form onSubmit={handleSubmit(onSubmit)}>
-          <Flex
-            w={"566px"}
-            justify={"space-between"}
-            gap={"32px"}
-            align="center"
-            mb={"40px"}
-          >
-            <Image
-              borderRadius="full"
-              boxSize="150px"
-              src={'http://173.212.221.237/images/'+watch('profilePhoto')}
-              alt="Dan Abramov"
-              w={"200px"}
-              height={"200px"}
-              objectFit={"contain"}
-              me={"20px"}
-              
-            />
-           
-           <FormControl mt="20px" isInvalid={!!errors.profilePhoto}>
-              <FormLabel
-                htmlFor="file"
-                color="#707070"
-                fontSize="18px"
-                width="100%"
-              >
-                <Box
-                  display="flex"
-                  alignItems="center"
-                  justifyContent="center"
-                  w="100%"
-                  cursor="pointer"
-                  textAlign="center"
-                  _hover={{ color: "white" }}
-                  p="12px 0"
-                  borderRadius="6px"
-                  border="1px solid #bababc"
-                  fontWeight="600"
-                  color="#fff"
-                  bg="#7848F4"
-                  gap="5px"
-                >
-                  <Image
-                    src="../assests/login/photo.png"
-                    alt="Upload"
-                    w="20px"
-                    h="20px"
-                    cursor="pointer"
-                  />
-                  <Text>Upload photo</Text>
-                </Box>
-              </FormLabel>
+            <Flex
+              w={"700px"}
+              justify={"space-between"}
+              gap={"80px"}
+              align="center"
+              mb={"40px"}
+            >
+              <Box>
+                <Image
+                  boxSize="120px"
+                  src={"http://173.212.221.237/images/" + watch("profilePhoto")}
+                  alt="Dan Abramov"
+                  margin="30px"
+                  borderRadius="full"
+                />
+              </Box>
 
-              <Controller
-                name="profilePhoto"
-                control={control}
-                render={({ field }) => (
-                  <Input
-                    {...field}
-                    value={undefined}
-                    id="file"
-                    display="none"
-                    type="file"
-                    accept="image/*"
-                  />
-                )}
-              />
-              <FormErrorMessage>
-                {errors?.profilePhoto?.message}
-              </FormErrorMessage>
-            </FormControl>
-          </Flex>
-         
+              <FormControl mt="20px" isInvalid={!!errors.profilePhoto}>
+                <FormLabel
+                  htmlFor="file"
+                  color="#707070"
+                  fontSize="18px"
+                  width="50%"
+                >
+                  <Box
+                    display="flex"
+                    alignItems="center"
+                    justifyContent="center"
+                    w="100%"
+                    cursor="pointer"
+                    textAlign="center"
+                    _hover={{ color: "white" }}
+                    p="12px 0"
+                    borderRadius="6px"
+                    border="1px solid #bababc"
+                    fontWeight="600"
+                    color="#fff"
+                    bg="#7848F4"
+                    gap="5px"
+                  >
+                    <Image
+                      src="../assests/login/photo.png"
+                      alt="Upload"
+                      w="20px"
+                      h="20px"
+                      cursor="pointer"
+                    />
+                    <Text>Upload photo</Text>
+                  </Box>
+                </FormLabel>
+
+                <Controller
+                  name="profilePhoto"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      {...field}
+                      value={undefined}
+                      id="file"
+                      display="none"
+                      type="file"
+                      accept="image/*"
+                    />
+                  )}
+                />
+                <FormErrorMessage>
+                  {errors?.profilePhoto?.message}
+                </FormErrorMessage>
+              </FormControl>
+            </Flex>
+
             <FormControl mb={"40px"} isInvalid={!!errors?.userName}>
               <FormLabel>USERNAME</FormLabel>
               <Controller
